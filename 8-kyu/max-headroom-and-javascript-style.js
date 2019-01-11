8cac4355bc805ddd6dfa8182c06df78f8a3fe63a
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-11 20:59:50 +0000

    wtf was this kata

---
+++ 8-kyu/max-headroom-and-javascript-style.js
@@ -0,0 +1,15 @@
+/*
+Link to kata: https://www.codewars.com/kata/max-headroom-and-javascript-style
+*/
+
+// Solution
+
+const getMax1 = () => {
+  let max = {name: 'Max Headroom'};
+  return max;
+  }
+  
+  const getMax2 = () => {
+  return {name: 'Max Headroom'};
+  }
+  
