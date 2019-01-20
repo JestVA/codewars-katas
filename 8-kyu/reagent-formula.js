dc2a16b0cc285062f060ba5da0c640dec8d4d8b6
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-20 13:07:12 +0000

    working solution for reagent formula using ifs

---
+++ 8-kyu/reagent-formula.js
@@ -0,0 +1,18 @@
+// LINK: https://www.codewars.com/kata/simple-fun-number-352-reagent-formula/train/javascript
+
+// SOLUTION: 
+
+const isValid = formula => {
+  //coding and coding..
+  if (formula.indexOf(1) > -1 && formula.indexOf(2) > -1) {
+    return false
+  } else if (formula.indexOf(3) > -1 && formula.indexOf(4) > -1) {
+    return false
+  } else if (formula.indexOf(7) > -1 || formula.indexOf(8) > -1) {
+    if (formula.indexOf(5) > -1 && formula.indexOf(6) > -1) {
+      return true
+    } else if (formula.indexOf(5) > -1 || formula.indexOf(6) > -1) {
+      return false
+    } 
+  } return true
+}
