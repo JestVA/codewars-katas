123e877d274efd63f6bd46d99c4330e6dc55501a
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-30 21:00:11 +0000

    lame kata

---
+++ 8-kyu/primitives-operator-precedence.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/noobcode-02-tricky-questions-primitives-and-operator-precedence/train/javascript
+
+const greaterThanLessThan = (a,b,c) => {
+  return a < c ? a < b < c : a < b < c
+}
+
