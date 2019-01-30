f2a2d1f21377810f6236e9c4432e51f79d1502e2
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-30 20:47:16 +0000

    grasshoper trick

---
+++ 8-kyu/grasshoper.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/grasshopper-order-of-operations/train/javascript
+*/
+
+// What I have learned
+
+// 1. How to trick solutions that ask for a certain value by using the length property of a string.
+'+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*'.length;
