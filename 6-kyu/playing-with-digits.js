dfc8725e8a101c6785243cac56b172ede5aeac8e
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-31 23:07:12 +0000

    Good lvl6 kata, return an object from map just like that

--- 6-kyu/playing-with-digits.js
+++ 6-kyu/playing-with-digits.js
@@ -1,2 +1,10 @@
 // https://www.codewars.com/kata/playing-with-digits/train/javascript
-
+const digPow = (n, p) => {
+  sumOfDigits = n.toString().split``.map((n, i) => {
+  return {
+number: n, 
+power: i+p
+}
+}).map(s => s.number ** s.power).reduce((a, c) => a + c)
+return sumOfDigits%n === 0 ? sumOfDigits / n : -1
+}
