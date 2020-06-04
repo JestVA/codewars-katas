9570ceac7bfcbe722ef94ebdd862942e8b74e85f
Dorin <d.dumitrascuta@gmail.com>
2020-06-04 01:24:32 +0100

    Still missing some key concepts here - Clojures?!

---
+++ 4-kyu/1-es5-generators-1.js
@@ -0,0 +1,53 @@
+/*
+ES5 Generators(i)
+https://www.codewars.com/kata/53c29a6abb5187180d000b65/train/javascript
+*/
+
+const generator = (sequencer, ...args) => {
+
+	// get the optional args
+	const opt = args.slice(0);
+	
+	let state = null;
+
+	return {
+		next: () => sequencer()(state), // think I need a clojure to update state
+	}
+}
+
+
+const dummySeq = () => () => "dummy";
+
+// 1, 1, 2, 6, 24, ...
+const factorialSeq = (current) => (current) => {
+	
+	console.log(current);
+	
+	// need to store state somehow 
+
+	let [prev, curr] = [0, 1];
+	for(;;) { // this empty condition is taken to evaluate to 1
+		[prev, curr] = [curr, prev + curr];
+		
+		// updating state variable here does not work 
+		current = 'something'
+		
+		return curr;
+	}
+
+}
+
+
+
+
+function fibonacciSeq() {
+}
+
+function rangeSeq(start, step) {
+}
+
+function primeSeq() {
+}
+
+function partialSumSeq() {
+}
