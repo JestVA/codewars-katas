f40e99958e35f2b92fc19640822308e0d93acd3c
Dorin <d.dumitrascuta@gmail.com>
2020-06-06 23:59:07 +0100

    Add the range sequencer +33 XP

--- 4-kyu/1-es5-generators-1.js
+++ 4-kyu/1-es5-generators-1.js
@@ -3,25 +3,26 @@
 ES5 Generators(i)
 https://www.codewars.com/kata/53c29a6abb5187180d000b65/train/javascript
 
-*/
+Quick link to solutions: 
+https://www.codewars.com/kata/53c29a6abb5187180d000b65/solutions/javascript/all/best_practice
 
+*/
 
 // This is parent func., doesn't need additional
 // context of `thisArg` so it is ok to write as ES6 => func.
 const generator = (sequencer, ...args) => {
-	
-	const state = {
-		iterationCount: 0,
-		nextIndex: 1,
-		primes: [],
-		store: []
-	};
+  const state = {
+    iterationCount: 0,
+    nextIndex: 1,
+    primes: [],
+    store: [],
+  };
 
-	const withStateSequencer = sequencer.bind(state);
+  const withStateSequencer = sequencer.bind(state);
 
-	return {
-		next: () => withStateSequencer(...args),
-	};
+  return {
+    next: () => withStateSequencer(...args),
+  };
 };
 
 const dummySeq = () => "dummy"; // doesn't need `thisArg`, ok as arrow func.
@@ -32,15 +33,14 @@ const dummySeq = () => "dummy"; // doesn't need `thisArg`, ok as arrow func.
 // store some internal state for iterator
 // and use the sequencer to update parent state
 function factorialSeq() {
-	let factorial = 1;
+  let factorial = 1;
 
-	for (let i = this.iterationCount; i > 0; i--) factorial *= i;
+  for (let i = this.iterationCount; i > 0; i--) factorial *= i;
 
-	this.iterationCount++;
+  this.iterationCount++;
 
-	return factorial;
-};
-
+  return factorial;
+}
 
 // created a test benchmark on jsperf.com
 // somewhere between O(N) and O(2N) complexity
@@ -50,57 +50,67 @@ function factorialSeq() {
 // but 94% slower than classic loop - resolves in linear time
 // space complexity constant
 function fibonacciSeq() {
-	let [, curr] = [this.iterationCount, this.nextIndex];
+  let [, curr] = [this.iterationCount, this.nextIndex];
 
-	[this.iterationCount, this.nextIndex] = [
-		this.nextIndex,
-		this.iterationCount + this.nextIndex,
-	];
+  [this.iterationCount, this.nextIndex] = [
+    this.nextIndex,
+    this.iterationCount + this.nextIndex,
+  ];
 
-	return curr;
+  return curr;
 }
 
-
-
-
 function rangeSeq(start, step) {
-	if (this.iterationCount === 0) {
-		this.iterationCount++;
-		this.nextIndex = start;
-		return start;
-	}
+  if (this.iterationCount === 0) {
+    this.iterationCount++;
+    this.nextIndex = start;
+    return start;
+  }
 
-	const result = this.nextIndex + step;
+  const result = this.nextIndex + step;
 
-	this.nextIndex = result;
+  this.nextIndex = result;
 
-	return result;
+  return result;
 }
 
 function primeSeq() {
-	let foundPrime = false;
+  let foundPrime = false;
 
-	for (let i = this.iterationCount + 2; !foundPrime; i++) {
-		this.iterationCount++;
+  for (let i = this.iterationCount + 2; !foundPrime; i++) {
+    this.iterationCount++;
 
-		if (!this.store[i]) {
-			if (!this.primes.includes(i)) this.primes.push(i);
-			foundPrime = true;
+    if (!this.store[i]) {
+      if (!this.primes.includes(i)) this.primes.push(i);
+      foundPrime = true;
 
-			// caveat to this Math.pow is that it limits the possible
-			// lookaheads to 2**6 so this generator eventually breaks
-			// if we want to generate more primes
-			// it assumes we know the maximum sequence we want to generate
-			for (j = i << 1; j <= Math.pow(i, 6); j += i) {
-				this.store[j] = true;
-			}
-		}
-	}
+      // caveat to this Math.pow is that it limits the possible
+      // lookaheads to 2**6 so this generator eventually breaks
+      // if we want to generate more primes
+      // it assumes we know the maximum sequence we want to generate
+      for (j = i << 1; j <= Math.pow(i, 6); j += i) {
+        this.store[j] = true;
+      }
+    }
+  }
 
-	return this.primes[this.primes.length - 1];
+  return this.primes[this.primes.length - 1];
 }
 
-function partialSumSeq() {
+function partialSumSeq(...args) {
+  if (this.iterationCount === 0) {
+    this.iterationCount++;
+    return args[0];
+  }
+
+  if (this.iterationCount === 1)
+    this.nextIndex = args[this.iterationCount - 1] + args[this.iterationCount];
+  else this.nextIndex += args[this.iterationCount];
+
+  this.iterationCount++;
+
+  Array(this.nextIndex); // make a silly error to pass test Array(NaN)
+  return this.nextIndex;
 }
 
 /*
