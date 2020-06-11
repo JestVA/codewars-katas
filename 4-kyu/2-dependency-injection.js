ef97a1dc807928f8fb5bdbb0570a7144ce775077
Dorin <d.dumitrascuta@gmail.com>
2020-06-11 23:13:53 +0100

    Getting there, need to arrange order of dependencies

--- 4-kyu/2-dependency-injection.js
+++ 4-kyu/2-dependency-injection.js
@@ -10,23 +10,25 @@
 var DI = function (dependency) {
 	this.dependency = dependency;
 	console.log(this.dependency);
+	
 };
 
 // Should return new function with resolved dependencies
-DI.prototype.inject =  (func) => {
+DI.prototype.inject =  function(resolve) {
+	console.log(resolve)
+	console.log(this.dependency);
+	const deps = [];
+	for(const dep of Object.keys(this.dependency))
+		deps.push(this.dependency[dep]);
+	return () => resolve(...deps); // got something here 
 	
+} 
 
-	
-	return function()
-	{
-
-		console.log(func)
-    return 'this is dep1 -> this is dep2 -> this is dep3' // this already passes the test lolz 
-	}
-};
 
 /*
 	Research:
 	Objects, object functions and prototypes
 	https://levelup.gitconnected.com/the-javascript-object-paradigm-and-prototypes-explained-simply-e9cb9eaa49aa#:~:text=When%20we%20create%20an%20object,actually%20instance%2Dobjects%20of%20Object%20.
+	https://stackoverflow.com/questions/53695772/javascript-access-class-instance-property-in-prototype-function
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of	
 */
