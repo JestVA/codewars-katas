9917c197465438d8ac64f4ea4836feed45fc581e
Dorin <d.dumitrascuta@gmail.com>
2020-06-13 13:23:37 +0100

    add synopsis and interesting regex, function call with apply args and bind

--- 4-kyu/2-dependency-injection.js
+++ 4-kyu/2-dependency-injection.js
@@ -1,5 +1,6 @@
 /*
-	https://www.codewars.com/kata/5302d655be2a91068b0001fb/train/javascript
+	Link => https://www.codewars.com/kata/5302d655be2a91068b0001fb/train/javascript
+	Solutions => https://www.codewars.com/kata/5302d655be2a91068b0001fb/solutions/javascript/all/clever
 */
 
 /**
@@ -30,4 +31,18 @@ DI.prototype.inject = function (resolve) {
 	https://levelup.gitconnected.com/the-javascript-object-paradigm-and-prototypes-explained-simply-e9cb9eaa49aa#:~:text=When%20we%20create%20an%20object,actually%20instance%2Dobjects%20of%20Object%20.
 	https://stackoverflow.com/questions/53695772/javascript-access-class-instance-property-in-prototype-function
 	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of	
+
+	Post solve analysis:
+
+	One of the solutions had this good regEx /^[^(]+\(([^)]+)/
+	- match beginning of string
+	- start negated char group, that does not match (
+	- match 1 or more of preceding token 
+	- escape (
+	- start new negated group that excludes )
+	- match 1 or more of preceding token  
+	
+	This way, using exec you end of with "arg1, arg2, arg3" in the [1] position of the exec []
+
+	
 */
